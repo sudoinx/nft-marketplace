657057f860ffd566dde5a2a49646643919e6d43c
st0mx <romangamers2009@gmail.com>
2025-09-12 16:30:02 +0300

    Update ++

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// function checkForm(el) {
+//   const mailto = el.email.value;
+//   console.log(mailto);
+
+//   let fail = "";
+
+//   if (email == "") {
+//     fail = "Заполните поле електронной почты!";
+//   } else if (email.length <= 1) {
+//     fail = "Електронная почта должна быть больше одного символа!";
+//   }
+
+//   return false;
+// }
+
+// function footerForm(el) {
+//   const mailto = document.getElementById("footer-email");
+//   console.log(mailto);
+
+//   return false;
+// }
